eb9a7dc979b9f2afff78a286d316ecfbf1bf743f
Anoop Joshi <anoopjoshi@hotmail.co.uk>
2018-08-11 00:31:09 +0100

    add incoming mails controller

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,2 +1,9 @@
 class IncomingMailsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def create
+    Rails.logger.debug params.inspect
+    Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
+  end
+  
 end
